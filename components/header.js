bbc0bf885477d3e7361e83168d444ccd3d7743e2
kito2007 <kito200742@gmail.com>
2023-04-19 18:45:35 +0900

    구글 아날리틱스 스크립트 추가

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Script from "next/script";
 import DarkModeToggleButton from "./dark-mode-toggle-button";
 
 export default function Header() {
@@ -26,8 +27,16 @@ export default function Header() {
                         </Link>
                     </nav>
                     {/*need creating dark mode toggle button*/}
-                    <DarkModeToggleButton/>
+                    <DarkModeToggleButton />
                 </div>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-8JB2T1Q0TS"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments)}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-8JB2T1Q0TS');
+                </script>
             </header>
         </>
     );
